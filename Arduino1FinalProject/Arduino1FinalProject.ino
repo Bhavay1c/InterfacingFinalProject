00a1c596062228e095a63ccf832fc9f383a80da3
BhavayGarg <bhavaygarg123@gmail.com>
2025-10-28 11:28:26 -0400

    Init commit stater

---
+++ Arduino1FinalProject/Arduino1FinalProject.ino
@@ -0,0 +1,20 @@
+/*
+Starting the final project Arduino 1 
+
+Membrane switch keypad (passcode & temperature input)
+LCD1602 display
+4-digit 7-segment display
+Active buzzer
+Push button (PB1)
+
+*/
+
+
+void setup() {
+
+
+}
+
+void loop() {
+
+}
---
+++ Arduino2FinalProject/Arduino2FinalProject.ino
@@ -0,0 +1,24 @@
+/* Starting Final project Arduino 2 
+(Sensor & Control Hub):
+
+PIR motion sensor
+DHT11 temperature/humidity sensor
+Ultrasonic sensor (HC-SR04)
+Photoresistor (light sensor)
+DC motor with fan blade
+Passive buzzer
+4 yellow LEDs
+4 red LEDs
+4-digit 7-segment display
+LCD1602 display
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
